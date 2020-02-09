a61e4fa354cb51efebe9ee831f6d18fb0b05147f
Novan <25704935+nofun97@users.noreply.github.com>
2020-02-09 21:30:07 +1100

    WIP: added newline

--- intset_benchmark_test.go
+++ intset_benchmark_test.go
@@ -23,12 +23,12 @@ func benchmarkWithIntSet(b *testing.B, n int) {
 	}
 }
 
-func BenchmarkNewIntSet100(b *testing.B) {benchmarkNewIntSet(b, 100)}
-func BenchmarkNewIntSet1k(b *testing.B) {benchmarkNewIntSet(b, 1000)}
-func BenchmarkNewIntSet100k(b *testing.B) {benchmarkNewIntSet(b, 100000)}
-func BenchmarkNewIntSet1M(b *testing.B) {benchmarkNewIntSet(b, 1000000)}
+func BenchmarkNewIntSet100(b *testing.B)  { benchmarkNewIntSet(b, 100) }
+func BenchmarkNewIntSet1k(b *testing.B)   { benchmarkNewIntSet(b, 1000) }
+func BenchmarkNewIntSet100k(b *testing.B) { benchmarkNewIntSet(b, 100000) }
+func BenchmarkNewIntSet1M(b *testing.B)   { benchmarkNewIntSet(b, 1000000) }
 
-func BenchmarkWithIntSet100(b *testing.B) {benchmarkWithIntSet(b, 100)}
-func BenchmarkWithIntSet1k(b *testing.B) {benchmarkWithIntSet(b, 1000)}
-func BenchmarkWithIntSet100k(b *testing.B) {benchmarkWithIntSet(b, 100000)}
-func BenchmarkWithIntSet1M(b *testing.B) {benchmarkWithIntSet(b, 1000000)}
+func BenchmarkWithIntSet100(b *testing.B)  { benchmarkWithIntSet(b, 100) }
+func BenchmarkWithIntSet1k(b *testing.B)   { benchmarkWithIntSet(b, 1000) }
+func BenchmarkWithIntSet100k(b *testing.B) { benchmarkWithIntSet(b, 100000) }
+func BenchmarkWithIntSet1M(b *testing.B)   { benchmarkWithIntSet(b, 1000000) }
